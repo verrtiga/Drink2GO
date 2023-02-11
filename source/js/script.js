592e3d5016aedf0f2aaa02adc96411d1009a99ec
verrtiga <vertegel0802@icloud.com>
2023-02-11 23:23:11 +0300

    доделал навигацию

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,22 @@
-/* в этот файл добавляет скрипты*/
+let nav = document.querySelector('.navigation__container');
+let toggle = document.querySelector('.header__toggle-menu');
+let head = document.querySelector('.header__toggle-menu');
+
+nav.classList.remove('navigation__container--nojs');
+head.classList.remove('header--nojs');
+
+toggle.addEventListener('click', function (){
+  if (nav.classList.contains('navigation__container--close')) {
+    nav.classList.remove('navigation__container--close');
+    nav.classList.add('navigation__container--open');
+    toggle.classList.remove('header__toggle-menu--open');
+    toggle.classList.add('header__toggle-menu--close');
+
+  } else {
+    nav.classList.add('navigation__container--close');
+    nav.classList.remove('navigation__container--open');
+    toggle.classList.add('header__toggle-menu--open');
+    toggle.classList.remove('header__toggle-menu--close');
+  }
+})
+
